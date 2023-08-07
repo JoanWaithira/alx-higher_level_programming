2697fea56e9bdebb5709fd1e905d799f2e4d88b7
JoanWaithira <joanwaithira.jw@gmail.com>
2023-08-07 11:40:08 -0700

    Write a Python script that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * check_cycle - Write a function in C that checks if a singly linked list has a cycle in it.
+ * @list: The stringly linked list to check
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+
