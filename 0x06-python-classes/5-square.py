54ddea987db05b4566ad34c7a7fd17be27bf6f5c
JoanWaithira <joanwaithira.jw@gmail.com>
2023-08-29 05:54:02 -0700

    Write a class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,4 +38,12 @@ class Square:
 
     def area(self):
         """Prints area of the square"""
-        return self.__size ** self.__size
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print instant attribute size"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
