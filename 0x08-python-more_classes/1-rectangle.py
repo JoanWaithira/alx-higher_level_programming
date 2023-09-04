17ea9682cfdf3e9bcc1f39c3e2fde22afc1cd1db
JoanWaithira <joanwaithira.jw@gmail.com>
2023-09-04 03:28:28 -0700

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,6 +2,8 @@
 """
 Define a rectangle class
 """
+
+
 class Rectangle:
     """A class that represents a rectangle"""
     def __init__(self, width=0, height=0):
@@ -12,8 +14,8 @@ class Rectangle:
         width (int): of the rectabgle
         height (int) of the rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
         @property
         def width(self):
