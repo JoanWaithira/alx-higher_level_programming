a86de5948db5d04bb62ffff8ddf75efb04a590b3
JoanWaithira <joanwaithira.jw@gmail.com>
2023-09-06 02:05:17 -0700

    Final work

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,4 +3,30 @@
 Unittests for max-integer
 """
 import unittest
-max_integer = __import('6-max_integer).max_integer
+max_integer = __import('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    """Test for max integer"""
+    def test_max_integer(self):
+        """Tests for the maximum lists"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_empty(self):
+        """If the list is empty """
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_negative(self):
+        """Tests for negtive values"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_max_mixed(self):
+        """Mixed both positive and negative"""
+        self.assertEqual(max_integer([-1, -2, -3, 4]), 4)
+
+    def test_max_one(self):
+        """Containing only one item """
+        self.assertEqual(max_integer([1]), 1)
+
+if __name = '__main__':
+    unittest.main()
