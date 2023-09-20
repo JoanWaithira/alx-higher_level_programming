23c8f8d83b4b563c7c92ea31b08b29e5b5824614
JoanWaithira <joanwaithira.jw@gmail.com>
2023-09-20 12:54:25 -0700

    Base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-    This module contains a class: BaseModel
+    This module contains a class: Base
 """
 import json
 import os
 import csv
 
 
-class BaseModel:
+class Base:
     """
         This class contains a private attribute
         and other methods
@@ -29,67 +29,67 @@ class BaseModel:
         if id is not None:
             self.id = id
         else:
-            BaseModel.__nb_objects += 1
-            self.id = BaseModel.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
-    def to_json_string(list_of_dicts):
-        """Returns JSON string representation of objects"""
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of object"""
 
-        if (list_of_dicts is None or len(list_of_dicts) == 0):
-            return "[]"
+        if (list_dictionaries is None or len(list_dictionaries) == 0):
+            return ("[]")
 
-        json_string = json.dumps([obj for obj in list_of_dicts])
+        json_string = json.dumps([obj for obj in list_dictionaries])
 
-        return json_string
+        return (json_string)
 
     @classmethod
-    def save_to_file(cls, list_of_objs):
-        """Saves JSON string representation of objects to a file"""
+    def save_to_file(cls, list_objs):
+        """Saves JSON string representation of objects to file"""
         class_name = cls.__name__  # get the class name
         filename = str(class_name) + ".json"
 
-        if (not isinstance(list_of_objs, list)):
-            string_to_save = BaseModel.to_json_string([])
+        if (not isinstance(list_objs, list)):
+            string_to_save = Base.to_json_string([])
 
-        elif (list_of_objs is None or len(list_of_objs) == 0):
-            string_to_save = BaseModel.to_json_string([])
+        elif (list_objs is None or len(list_objs) == 0):
+            string_to_save = Base.to_json_string([])
         else:
             """
-                Convert to dictionary representation
+                convert to dictionary representation
                 and perform JSON operation
             """
-            for obj in list_of_objs:
-                if not issubclass(type(obj), BaseModel):
+            for obj in list_objs:
+                if not issubclass(type(obj), Base):
                     raise TypeError
 
-            string_to_save = BaseModel.to_json_string([obj.to_dictionary()
-                                                       for obj in list_of_objs])
-        # Save/write to a file
+            string_to_save = Base.to_json_string([obj.to_dictionary()
+                                                 for obj in list_objs])
+        # save/write to file
         with open(filename, "w+", encoding="utf-8") as class_file:
             class_file.write(string_to_save)
 
     @staticmethod
     def from_json_string(json_string):
-        """Returns the Python object of JSON string representation"""
-        return json.loads(json_string)
+        """Returns the python object of json string representation"""
+        return (json.loads(json_string))
 
     @classmethod
     def create(cls, **dictionary):
-        """Creates a new instance of class cls"""
+        """creates a new instance of class cls"""
         class_name = cls.__name__
 
         if class_name == "Rectangle":
-            from models.rectangle import RectangleModel
-            dummy = RectangleModel(6, 7, 0, 0)
+            from models.rectangle import Rectangle
+            dummy = Rectangle(6, 7, 0, 0)
             dummy.update(**dictionary)
-            return dummy
+            return (dummy)
 
         if class_name == "Square":
-            from models.square import SquareModel
-            dummy = SquareModel(5, 0, 0)
+            from models.square import Square
+            dummy = Square(5, 0, 0)
             dummy.update(**dictionary)
-            return dummy
+            return (dummy)
 
     @classmethod
     def load_from_file(cls):
@@ -101,49 +101,49 @@ class BaseModel:
         if class_name == "Rectangle":
             filename = "./Rectangle.json"
             if os.path.exists(filename):
-                from models.rectangle import RectangleModel
+                from models.rectangle import Rectangle
                 with open(filename, "r+", encoding="utf-8") as json_file:
-                    json_string = json_file.read()  # Read file
-                list_of_dict = BaseModel.from_json_string(json_string)
-                list_of_instances = [RectangleModel.create(**dic)
+                    json_string = json_file.read()  # read file
+                list_of_dict = Base.from_json_string(json_string)
+                list_of_instances = [Rectangle.create(**dic)
                                      for dic in list_of_dict]
-                return list_of_instances
+                return (list_of_instances)
 
         elif class_name == "Square":
-            from models.square import SquareModel
+            from models.square import Square
             filename = "./Square.json"
             if os.path.exists(filename):
                 with open(filename, "r+", encoding="utf-8") as json_file:
-                    json_string = json_file.read()  # Read file
-                list_of_dict = BaseModel.from_json_string(json_string)
-                list_of_instances = [SquareModel.create(**dic)
+                    json_string = json_file.read()  # read file
+                list_of_dict = Base.from_json_string(json_string)
+                list_of_instances = [Square.create(**dic)
                                      for dic in list_of_dict]
-                return list_of_instances
+                return (list_of_instances)
 
-        return []
+        return ([])
 
     @classmethod
-    def save_to_file_csv(cls, list_of_objs):
+    def save_to_file_csv(cls, list_objs):
         """
-            Creates a CSV type file of class.__name__ and writes
-            to it in CSV formatted form.
+            Creates a csv type file of class.__name__ and writes
+            to it in csv formatted form.
         """
         class_name = cls.__name__
         filename = str(cls.__name__) + ".csv"
 
-        if not isinstance(list_of_objs, list):
+        if not isinstance(list_objs, list):
             list_to_iter = []
 
-        elif list_of_objs is None or len(list_of_objs) == 0:
+        elif list_objs is None or len(list_objs) == 0:
             list_to_iter = []
 
         else:
 
-            for obj in list_of_objs:
-                if not issubclass(type(obj), BaseModel):
+            for obj in list_objs:
+                if not issubclass(type(obj), Base):
                     raise TypeError
 
-            list_to_iter = [obj.to_dictionary() for obj in list_of_objs]
+            list_to_iter = [obj.to_dictionary() for obj in list_objs]
 
         if class_name == "Rectangle":
             with open(filename, "w", newline='') as csvfile:
@@ -168,7 +168,7 @@ class BaseModel:
 
     @classmethod
     def load_from_file_csv(cls):
-        """Loads a CSV file and returns instances of cls class"""
+        """Loads a csv file and returns instances of cls class"""
         # Get class name
         class_name = cls.__name__
 
@@ -176,14 +176,14 @@ class BaseModel:
         if class_name == "Rectangle":
             filename = "./Rectangle.csv"
             if os.path.exists(filename):
-                from models.rectangle import RectangleModel
+                from models.rectangle import Rectangle
 
                 with open(filename, "r", newline="") as csvfile:
                     fieldnames = ["id", "width", "height", "x", "y"]
                     """
                         The fieldnames are ignored because the DictReader
                         class uses the values in the first row of the file
-                        as fieldnames, and it returns each row in a dictionary
+                        as fieldnames, and it returns eash row in a dictionary
                     """
                     list_of_dict = list(csv.DictReader(csvfile,
                                         fieldnames=None))
@@ -195,14 +195,14 @@ class BaseModel:
                         for key in dic:
                             dic[key] = int(dic[key])
 
-                list_of_instances = [RectangleModel.create(**dic)
+                list_of_instances = [Rectangle.create(**dic)
                                      for dic in list_of_dict]
-                return list_of_instances
+                return (list_of_instances)
 
         elif class_name == "Square":
             filename = "./Square.csv"
             if os.path.exists(filename):
-                from models.square import SquareModel
+                from models.square import Square
 
                 with open(filename, "r", newline="") as csvfile:
 
@@ -212,9 +212,8 @@ class BaseModel:
                         for key in dic:
                             dic[key] = int(dic[key])
 
-                list_of_instances = [SquareModel.create(**dic)
+                list_of_instances = [Square.create(**dic)
                                      for dic in list_of_dict]
-                return list_of_instances
+                return (list_of_instances)
 
-        return []
-
+        return ([])
