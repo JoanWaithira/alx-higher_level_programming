8e532c4ddc2e16573e09156d4d094cbcd489f5b3
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-09 00:29:50 -0700

    Square property

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,18 +5,15 @@
 class Square:
     """Defining a square by size"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """ Initialise a new square.
 
         Args:
         size (int): Size of square
+        position: Coordinates
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -35,6 +32,21 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position (self, value):
+        """ Setter method for position
+        Args:
+        Position: Coordinates
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i,int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
         """Prints area of the square"""
