dfd8530d26ca2bee83f10ff574c067071dbbeb80
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-17 01:23:16 -0700

    The solutions to the questions

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+--a script that lists all records of the table second_table
+SELET score, name FROM second_table ORDER BY SCORE DESC;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+--a script that lists all rows of the table firsttable
+SELECT * FROM first_table;
