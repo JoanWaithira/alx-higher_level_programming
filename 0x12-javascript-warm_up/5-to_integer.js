eb617ab7829ca56fc0d71326c6119723ce1ea343
JoanWaithira <joanwaithira.jw@gmail.com>
2023-11-13 03:53:58 -0800

    Is integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+const convertedNumber = parseInt(arg);
+
+if (isNan(number)) {
+	console.log('Not a number');
+} else {
+	console.log(`My number: ${number}`);
+}
