cbadb2571044078a84759f0171e34caf3aaa71ec
JoanWaithira <joanwaithira.jw@gmail.com>
2023-11-18 12:44:33 -0800

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cursor.fetchall()
     for row in rows:
-        print(row)
+        if row[1].startswith("N"):
+            print(row)
     cursor.close()
     db.close()
