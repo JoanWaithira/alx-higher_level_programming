3d843bdff0303c84882ecb29d13fa33f50aa92e4
JoanWaithira <joanwaithira.jw@gmail.com>
2023-12-01 05:09:52 -0800

    Response header value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL, sends a request to the URL """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
