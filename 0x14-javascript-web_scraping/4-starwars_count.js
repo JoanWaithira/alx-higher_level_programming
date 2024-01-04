c6a99e7407805acadf35991c5841998be55743d4
JoanWaithira <joanwaithira.jw@gmail.com>
2024-01-04 00:11:52 -0800

    Starwars Count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmsData = JSON.parse(body).results;
+    const count = filmsData.reduce((total, film) => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        return total + 1;
+      }
+      return total;
+    }, 0);
+
+    console.log(count);
+  }
+});
